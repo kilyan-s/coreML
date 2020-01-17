a950f7a6828bb10c8eaf12f846f17f9a839b2223
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-17 16:16:44 -0500

    Use AVFoundation to capture and back display camera feed in the view.

--- image-detection/image-detection/Controller/CameraVC.swift
+++ image-detection/image-detection/Controller/CameraVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
     //MARK: Outlets
@@ -18,8 +19,63 @@ class CameraVC: UIViewController {
     @IBOutlet weak var cameraView: UIView!
     
     
+    //MARK: Var
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    //MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Instantiate session and select resolution
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        //Get the back camera
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            //try to create an input using the back camera
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            //Try to add input to captureSession
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            
+            //Create output
+            cameraOutput = AVCapturePhotoOutput()
+            //Try to add output to captureSession
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput)
+                
+                //Create a video preview layer from the capture session
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                //Maintain aspect ratio & select orientation
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                //Add the previewLayer to the UIView
+                cameraView.layer.addSublayer(previewLayer!)
+                
+                //Start capturing camera feed
+                captureSession.startRunning()
+                
+            }
+            
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
     }
 
 

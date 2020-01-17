0630ed50e5715aa2a146e6b429b476032cb624a3
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-17 15:12:16 -0500

    Xcode project setup.

---
+++ image-detection/image-detection/Controller/CameraVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image-detection
+//
+//  Created by Kilyan SOCKALINGUM on 17/01/2020.
+//  Copyright © 2020 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
